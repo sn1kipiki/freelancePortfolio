1129e9577a23622ee1b32240e33ff2b9441251de
sn1kipiki <yurazayarnuk@gmail.com>
2023-01-20 22:17:26 +0200

    first com

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("#www")
+console.log(button)
+// const nav = document.querySelector(".header__nav")
+
+
+// button.addEventListener("click", () => {
+//     console.log("sdsd")
+// })
+
+AOS.init();
